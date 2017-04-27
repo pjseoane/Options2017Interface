6fada1fab5accc21b09d6703c664f1d771b5e464
pjseoane@gmail.com <Paulino@VAIO>
2017-04-26 22:45:05 -0300

    Agregado como clase, sin uso por ahora

--- src/Option2017Interface/impliedVolatility.java
+++ src/Option2017Interface/impliedVolatility.java
@@ -9,59 +9,60 @@ package Option2017Interface;
  *
  * @author Paulino
  */
-public class impliedVolatility extends OptionParameters{
+public class impliedVolatility extends AbstractOptionClass2017{
+    
+    //private AbstractOptionClass2017 option;
+    
     //Constructors
-    
-    public impliedVolatility(OptionParameters opt){
-    tipoContrato               =opt.tipoContrato;
-    underlyingValue            =opt.underlyingValue;
-    underlyingHistVolatility   =opt.underlyingHistVolatility;
-    dividendRate               =opt.dividendRate;
-    callPut                    =opt.callPut;
-    strike                     =opt.strike;
-    daysToExpiration           =opt.daysToExpiration;
-    tasa                       =opt.tasa;
-    impliedVol                 =opt.impliedVol;
-    optionMktValue             =opt.optionMktValue;
+    public impliedVolatility(AbstractOptionClass2017 opt){
+        tipoContrato               =opt.tipoContrato;
+        underlyingValue            =opt.underlyingValue;
+        underlyingHistVolatility   =opt.underlyingHistVolatility;
+        dividendRate               =opt.dividendRate;
+        callPut                    =opt.callPut;
+        strike                     =opt.strike;
+        daysToExpiration           =opt.daysToExpiration;
+        tasa                       =opt.tasa;
+        impliedVol                 =opt.impliedVol;
+        optionMktValue             =opt.optionMktValue;
     }
-    /*
-    public impliedVolatility(char tipoContrato, double underlyingValue,double underlyingHistVolatility,double dividendRate,char callPut, double strike,double daysToExpiration,double tasa,double impliedVol,double optionMktValue){
-    this.tipoContrato               =tipoContrato;
-    this.underlyingValue            =underlyingValue;
-    this.underlyingHistVolatility   =underlyingHistVolatility;
-    this.dividendRate               =dividendRate;
-    this.callPut                    =callPut;
-    this.strike                     =strike;
-    this.daysToExpiration           =daysToExpiration;
-    this.tasa                       =tasa;
-    this.impliedVol                 =impliedVol;
-    this.optionMktValue             =optionMktValue;
-    
+    public impliedVolatility (char tipoContrato, double underlyingValue,double underlyingHistVolatility,double dividendRate,char callPut, double strike,double daysToExpiration,double tasa,double impliedVol,double optionMktValue){
+        this.tipoContrato               =tipoContrato;
+        this.underlyingValue            =underlyingValue;
+        this.underlyingHistVolatility   =underlyingHistVolatility;
+        this.dividendRate               =dividendRate;
+        this.callPut                    =callPut;
+        this.strike                     =strike;
+        this.daysToExpiration           =daysToExpiration;
+        this.tasa                       =tasa;
+        this.impliedVol                 =impliedVol;
+        this.optionMktValue             =optionMktValue;
     }
+   
     
-    public double implied(){
+    public void runModel(){
         int contador=0;
         double dif;
-        double Accuracy=0.000001;
+        double accuracy=0.000001;
         dif=optionMktValue-prima;      
         
         double newVlt=impliedVol;
         
-         while(Math.abs(dif) > Accuracy && contador <20 && vega>0.000001 && optionMktValue>0 && newVlt>0.005){
+         while(Math.abs(dif) > accuracy && contador <20 && this.getVega()>0.000001 && this.optionMktValue>0 && newVlt>0.005){
          //newVlt+=(dif/vega/100);
             
                 //Asignamos la nueva vlt calculada a la opcion en cuestion y la recalculamos
                 //Utiliza el modelo correspondiente a esa opcion
                 //setOptionVlt(newVlt);
-                impliedVol+=(dif/vega/100);
-                opt.runModel();
+                impliedVol+=(dif/getVega()/100);
+                runModel();
                 dif=optionMktValue-prima;
                 contador++;
          }
          DerivativesArray[0][6]=impliedVol;
-         return impliedVol;
+        // return option.impliedVol;
          //return contador;
-        return 12.4;
-    }
-   */
+       
+    }//end method implied2
+   
 }
